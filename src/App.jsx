572d8c63b1d7528803d573730fb2d66e72ce0f01
tolibaevRinat <webtolibaevrinat@gmail.com>
2025-02-13 23:38:37 +0500

    13-02-2025 23-38

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
-import Preloader from './components/Preloader'
-
-const Home = React.lazy(() => import('./pages/Home'))
-const Header = React.lazy(() => import('./components/Header'))
-const Address = React.lazy(() => import('./components/Address'))
-const SchoolHead = React.lazy(() => import('./components/SchoolHead'))
-const DeputyHeadmaster = React.lazy(() =>
-	import('./components/DeputyHeadmaster')
-)
-const Psychologist = React.lazy(() => import('./components/Psyschologist'))
-const SchoolPassport = React.lazy(() => import('./components/SchoolPassport'))
-const SchoolCharter = React.lazy(() => import('./components/SchoolCharter'))
+// import Preloader from './components/Preloader'
 
 const App = () => {
 	return (
 		<>
-			<React.Suspense fallback={<Preloader />}>
-				<Header />
-			</React.Suspense>
-			<main className='page'>
-				<Routes>
-					<Route
-						path='/'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<Home />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/address'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<Address />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/schoolhead'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<SchoolHead />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/deputyheadmaster'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<DeputyHeadmaster />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/psychologists'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<Psychologist />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/schoolpassport'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<SchoolPassport />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path='/schoolcharter'
-						element={
-							<React.Suspense fallback={<Preloader />}>
-								<SchoolCharter />
-							</React.Suspense>
-						}
-					/>
-					<Route path='*' element={<h1>Not Found</h1>} />
-				</Routes>
-			</main>
+			<Header />
+			<main className='page container'></main>
 		</>
 	)
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,230 +1,115 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
+import { Dropdown } from 'rsuite'
 
 import styles from './Header.module.scss'
-import SelectLang from '../SelectLang'
 
 const Header = () => {
-	const { t, i18n } = useTranslation()
-
-	const [isTouchMenuButton, setIsTouchMenuButton] = React.useState(false)
-
-	const adminsItems = [
-		{
-			value: 'head teacher',
-			path: `/schoolhead`,
-		},
-		{
-			value: 'deputy headmaster',
-			path: '/deputyheadmaster',
-		},
-		{
-			value: 'psychologists',
-			path: '/psychologists',
-		},
-	]
-
-	const aboutSchool = [
-		{
-			value: 'School passport',
-			path: '/schoolpassport',
-		},
-		{
-			value: 'School Charter',
-			path: '/schoolcharter',
-		},
-	]
-
-	const symbols = {
-		en: [
-			{
-				value: 'Coat of arms of Uzbekistan',
-				url: 'https://en.wikipedia.org/wiki/Coat_of_arms_of_Uzbekistan',
-			},
-			{
-				value: 'Flag of Uzbekistan',
-				url: 'https://en.wikipedia.org/wiki/Flag_of_Uzbekistan',
-			},
-			{
-				value: 'Anthem of Uzbekistan',
-				url: 'https://en.wikipedia.org/wiki/National_anthem_of_Uzbekistan',
-			},
-			{
-				value: 'Coat of arms of Karakalpakstan',
-				url: 'https://en.wikipedia.org/wiki/Karakalpakstan',
-			}, // No dedicated page, link to the region
-			{
-				value: 'Flag of Karakalpakstan',
-				url: 'https://en.wikipedia.org/wiki/Karakalpakstan',
-			}, // No dedicated page, link to the region
-			{
-				value: 'Anthem of Karakalpakstan',
-				url: 'https://en.wikipedia.org/wiki/Karakalpakstan',
-			}, // No dedicated page, link to the region
-		],
-		ru: [
-			{
-				value: 'Герб Узбекистана',
-				url: 'https://ru.wikipedia.org/wiki/Герб_Узбекистана',
-			},
-			{
-				value: 'Флаг Узбекистана',
-				url: 'https://ru.wikipedia.org/wiki/Флаг_Узбекистана',
-			},
-			{
-				value: 'Гимн Узбекистана',
-				url: 'https://ru.wikipedia.org/wiki/Гимн_Узбекистана',
-			},
-			{
-				value: 'Герб Каракалпакстана',
-				url: 'https://ru.wikipedia.org/wiki/Герб_Каракалпакстана',
-			},
-			{
-				value: 'Флаг Каракалпакстана',
-				url: 'https://ru.wikipedia.org/wiki/Флаг_Каракалпакстана',
-			},
-			{
-				value: 'Гимн Каракалпакстана',
-				url: 'https://ru.wikipedia.org/wiki/Гимн_Каракалпакстана',
-			},
-		],
-		uz: [
-			// Example - you'll need to find the correct URLs
-			{
-				value: 'Oʻzbekiston gerbi',
-				url: 'https://uz.wikipedia.org/wiki/Oʻzbekiston_gerbi',
-			},
-			{
-				value: 'Oʻzbekiston bayrogʻi',
-				url: 'https://uz.wikipedia.org/wiki/Oʻzbekiston_bayrogʻi',
-			},
-			{
-				value: 'Oʻzbekiston madhiyasi',
-				url: 'https://uz.wikipedia.org/wiki/Oʻzbekiston_madhiyasi',
-			},
-			{
-				value: 'Qoraqalpogʻiston gerbi',
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: 'Qoraqalpogʻiston bayrogʻi',
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: 'Qoraqalpogʻiston madhiyasi',
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-		],
-		qr: [
-			{
-				value: "O'zbekistan gerbi",
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: "O'zbekistan bayrag'i",
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: "O'zbekistan gimni",
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: 'Qaraqalpaqistan gerbi',
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: "Qaraqalpaqistan bayrag'i",
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-			{
-				value: 'Qaraqalpaqistan gimni',
-				url: 'https://uz.wikipedia.org/wiki/Qoraqalpogʻiston',
-			}, // Find correct URLs
-		],
-	}
-
-	const currentLanguage = i18n.language.split('-')[0]
-
-	const symbolsToDisplay = symbols[currentLanguage] || symbols['en']
-
 	return (
 		<header className={styles.root}>
 			<div className={`${styles.inner} container`}>
 				<Link className={styles.logo} to='/'>
 					<img src='/icons/logo.png' alt='logo' />
 				</Link>
-				<nav
-					className={`${styles.nav} ${
-						isTouchMenuButton ? styles.isActive : ''
-					}`}
-				>
+				<nav className={styles.nav}>
 					<ul className={styles.list}>
 						<li className={styles.listItem}>
-							<button className={styles.dropdownButton} type='button'>
-								{t('Administration')}
-							</button>
-							<ul className={styles.subList}>
-								{adminsItems.map((obj, i) => (
-									<li key={i} className={styles.subListItem}>
-										<Link className={styles.subListLink} to={obj.path}>
-											{t(obj.value)}
-										</Link>
-									</li>
-								))}
-							</ul>
+							<Dropdown
+								className={styles.dropdown}
+								title='Администрация'
+								trigger='hover'
+							>
+								<Dropdown.Item>
+									<Link to='/'>Директор школы</Link>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<Link to='/'>Зам. директора школы</Link>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<Link to='/'>Психологи</Link>
+								</Dropdown.Item>
+							</Dropdown>
 						</li>
 						<li className={styles.listItem}>
-							<button className={styles.dropdownButton} type='button'>
-								{t('About school')}
-							</button>
-							<ul className={styles.subList}>
-								{aboutSchool.map((obj, i) => (
-									<li key={i} className={styles.subListItem}>
-										<Link className={styles.subListLink} to={obj.path}>
-											{t(obj.value)}
-										</Link>
-									</li>
-								))}
-							</ul>
+							<Dropdown
+								className={styles.dropdown}
+								title='О школе'
+								trigger='hover'
+							>
+								<Dropdown.Item>
+									<Link to='/'>Мероприятия</Link>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<Link to='/'>Паспорт школы</Link>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<Link to='/'>Устав школы</Link>
+								</Dropdown.Item>
+							</Dropdown>
 						</li>
 						<li className={styles.listItem}>
-							<button className={styles.dropdownButton} type='button'>
-								{t('Symbolism')}
-							</button>
-							<ul className={styles.subList}>
-								{symbolsToDisplay.map((obj, i) => (
-									<li key={i} className={styles.subListItem}>
-										<a
-											className={styles.link}
-											href={obj.url}
-											target='_blank'
-											rel='noopener noreferrer'
-										>
-											{t(obj.value)}
-										</a>
-									</li>
-								))}
-							</ul>
+							<Dropdown
+								className={styles.dropdown}
+								title='Символика'
+								trigger='hover'
+							>
+								<Dropdown.Item>
+									<a href='/'>Герб Узбекистана</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a href='/'>Флаг Узбекистана</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a href='/'>Гимн Узбекистана</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a href='/'>Герб Каракалпакстана</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a href='/'>Флаг Каракалпакстана</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a href='/'>Гимн Каракалпакстана</a>
+								</Dropdown.Item>
+							</Dropdown>
 						</li>
 						<li className={styles.listItem}>
-							<Link className={styles.button} to='/address'>
-								{t('Address')}
+							<Link className={styles.address} to='/'>
+								Адрес
 							</Link>
 						</li>
 					</ul>
 				</nav>
-				<SelectLang />
-				<button
-					onClick={() => setIsTouchMenuButton(!isTouchMenuButton)}
-					className={`${styles.burgerButton} ${
-						isTouchMenuButton ? styles.isActive : ''
-					}`}
-					type='button'
+				<Dropdown
+					className={styles.langDropdown}
+					title='Выберите язык'
+					trigger='hover'
 				>
-					{[...new Array(3)].map((_, i) => (
-						<span key={i} className={styles.line}></span>
-					))}
+					<Dropdown.Item>
+						<button className={styles.langBtn} type='button'>
+							Английский
+						</button>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<button className={styles.langBtn} type='button'>
+							Русский
+						</button>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<button className={styles.langBtn} type='button'>
+							Узбекский
+						</button>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<button className={styles.langBtn} type='button'>
+							Каракалпакский
+						</button>
+					</Dropdown.Item>
+				</Dropdown>
+				<button className={`${styles.menuButton} visible-mobile`} type='button'>
+					<span></span>
+					<span></span>
+					<span></span>
 				</button>
 			</div>
 		</header>
